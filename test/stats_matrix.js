a0fb977ccae26e169a96d72dec68a91a82365188
hitsujiwool <utatanenohibi@gmail.com>
2016-03-24 21:18:41 +0900

    StatsMatrix#colAt, #rowAtが返す配列の要素をオブジェクトに変更

--- test/stats_matrix.js
+++ test/stats_matrix.js
@@ -22,14 +22,36 @@ describe('StatMatrix', () => {
   });
 
   describe('#rowAt(id)', function() {
-    it('a row specified by id', () => {
-      assert.deepEqual(mat.rowAt('ryo@uniba.jp'), [0, 4.1666666865349, 0]);
+    it('return a row specified by id', () => {
+      assert.deepEqual(
+        mat.rowAt('ryo@uniba.jp'),
+        [
+          { header: '2016-02-08T00:00:00+09:00', value: 0 },
+          { header: '2016-02-15T00:00:00+09:00', value: 4.1666666865349 },
+          { header: '2016-02-22T00:00:00+09:00', value: 0 }
+        ]);
+    });
+
+    it('return null for unknown id', () => {
+      assert.strictEqual(mat.rowAt('baaa'), null);
     });
   });
 
   describe('#colAt(header)', function() {
     it('a column specified by header', () => {
-      assert.deepEqual(mat.colAt('2016-02-15T00:00:00+09:00'), [4.1666666865349, 3.1666666865349, 0.66666668653488, 5.7291666865349, 20.25]);
+      assert.deepEqual(
+        mat.colAt('2016-02-15T00:00:00+09:00'),
+        [
+          { rowName: 'ryo@uniba.jp', value: 4.1666666865349 },
+          { rowName: 'sei@uniba.jp', value: 3.1666666865349 },
+          { rowName: 'seiya@uniba.jp', value: 0.66666668653488 },
+          { rowName: 'haruma@uniba.jp', value: 5.7291666865349 },
+          { rowName: 'yumi@uniba.jp', value: 20.25 }
+        ]);
+    });
+
+    it('return null for unknown header', () => {
+      assert.strictEqual(mat.colAt('baaa'), null);
     });
   });
 });
